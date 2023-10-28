0da19ac6deebec753eb15fe3715a64932d72b322
FabulousStarfish <71825170+FabulousStarfish@users.noreply.github.com>
2023-10-28 10:57:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let startBtn = document.getElementById('start');
 let stopBtn = document.getElementById('stop');
 let resetBtn = document.getElementById('reset');
 
-let hour = 0;
 let minute = 0;
 let second = 0;
 let count = 0;
@@ -19,20 +18,34 @@ stopBtn.addEventListener('click', function () {
 
 resetBtn.addEventListener('click', function () {
 	timer = false;
-	hour = 0;
 	minute = 0;
 	second = 0;
 	count = 0;
-	document.getElementById('hr').innerHTML = "00";
 	document.getElementById('min').innerHTML = "00";
 	document.getElementById('sec').innerHTML = "00";
 	document.getElementById('count').innerHTML = "00";
+	document.getElementById("life").style.backgroundColor = "white";
+	const digits = document.getElementsByClassName("digit");
+	for (const box of digits) {
+		box.style.color = "#393951";
+	}
+	const txt = document.getElementsByClassName("txt");
+	for (const tx of txt) {
+		tx.style.color = "#393951";
+	}
 });
 
 function stopWatch() {
 	if (timer) {	
-		turn_on_light();	
-		document.getElementById("body").style.backgroundColor = "red";
+		document.getElementById("life").style.backgroundColor = "black";
+		const digits = document.getElementsByClassName("digit");
+		for (const box of digits) {
+			box.style.color = "white";
+		}
+		const txt = document.getElementsByClassName("txt");
+		for (const tx of txt) {
+			tx.style.color = "white";
+		}
 		count++;
 
 		if (count == 100) {
@@ -46,20 +59,12 @@ function stopWatch() {
 		}
 
 		if (minute == 60) {
-			hour++;
 			minute = 0;
 			second = 0;
 		}
-
-		let hrString = hour;
 		let minString = minute;
 		let secString = second;
 		let countString = count;
-
-		if (hour < 10) {
-			hrString = "0" + hrString;
-		}
-
 		if (minute < 10) {
 			minString = "0" + minString;
 		}
@@ -71,8 +76,6 @@ function stopWatch() {
 		if (count < 10) {
 			countString = "0" + countString;
 		}
-
-		document.getElementById('hr').innerHTML = hrString;
 		document.getElementById('min').innerHTML = minString;
 		document.getElementById('sec').innerHTML = secString;
 		document.getElementById('count').innerHTML = countString;
@@ -80,50 +83,3 @@ function stopWatch() {
 	}
 }
 
-function turn_on_light(){
-	const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
-
-if (SUPPORTS_MEDIA_DEVICES) {
-  //Get the environment camera (usually the second one)
-  navigator.mediaDevices.enumerateDevices().then(devices => {
-  
-    const cameras = devices.filter((device) => device.kind === 'videoinput');
-
-    if (cameras.length === 0) {
-      throw 'No camera found on this device.';
-    }
-    const camera = cameras[cameras.length - 1];
-
-    // Create stream and get video track
-    navigator.mediaDevices.getUserMedia({
-      video: {
-        deviceId: camera.deviceId,
-        facingMode: ['user', 'environment'],
-        height: {ideal: 1080},
-        width: {ideal: 1920}
-      }
-    }).then(stream => {
-      const track = stream.getVideoTracks()[0];
-
-      //Create image capture object and get camera capabilities
-      const imageCapture = new ImageCapture(track)
-      const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
-
-        //todo: check if camera has a torch
-
-        //let there be light!
-        const btn = document.querySelector('.switch');
-        btn.addEventListener('click', function(){
-          track.applyConstraints({
-            advanced: [{torch: true}]
-          });
-        });
-      });
-    });
-  });
-  
-  //The light will be on as long the track exists
-  
-  
-}
-}
